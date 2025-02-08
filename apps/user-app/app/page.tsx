72b67227c81a35bd674aaa3b47a51e7cf0384176
rohitxess <xessro@gmail.com>
2025-02-08 19:49:13 +0530

    installed tailwindcss libraries and cleared the initial code

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="text-xl text-gray-900 dark:text-grey">
+      hi there from home 
+    </div>
+  );
+}
---
+++ apps/user-app/postcss.config.js
@@ -0,0 +1,15 @@
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+
+
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+export default config;
